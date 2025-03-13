45beb896d655f3b0aaba263232875a9e8f075c2e
Alistair Spragg <alistair.spragg@gmail.com>
2025-03-13 23:18:02 +0000

    feat: more player endpoints

--- Wom.Net/Services/Players/PlayersService.cs
+++ Wom.Net/Services/Players/PlayersService.cs
@@ -2,7 +2,12 @@ using Functional.Sharp.Extensions;
 using Functional.Sharp.Helpers;
 using Functional.Sharp.Monads;
 using Wom.Net.Contracts;
+using Wom.Net.Enums;
 using Wom.Net.Extensions;
+using Wom.Net.Services.Competitions.Entities;
+using Wom.Net.Services.Competitions.Enums;
+using Wom.Net.Services.Groups.Entities;
+using Wom.Net.Services.Players.Entities;
 using Wom.Net.Services.Players.Responses;
 using Wom.Net.Utils;
 
@@ -95,5 +100,84 @@ internal class PlayersService(HttpClient client) : IPlayersService
             .WithRoute("progress")
             .ExecuteGetAsync(cancellationToken: cancellationToken)
             .ToTypedResultAsync<IEnumerable<GetAchievementProgressResponse>>(cancellationToken)
+    ).FlattenAsync();
+
+    public async Task<Result<IEnumerable<PlayerParticipation>>> Competitions(
+        string username,
+        CompetitionStatus? competitionStatus = null,
+        int? limit = null,
+        int? offset = null,
+        CancellationToken cancellationToken = default
+    ) => await Try.ExecuteAsync(async () =>
+        await new HttpClientBuilder(client)
+            .WithRoute("players")
+            .WithRoute(username)
+            .WithRoute("competitions")
+            .WithQueryParameter("status", competitionStatus?.ToString())
+            .WithQueryParameter("limit", limit)
+            .WithQueryParameter("offset", offset)
+            .ExecuteGetAsync(cancellationToken: cancellationToken)
+            .ToTypedResultAsync<IEnumerable<PlayerParticipation>>(cancellationToken)
+    ).FlattenAsync();
+
+    public async Task<Result<IEnumerable<PlayerCompetitionStanding>>> CompetitionStandings(
+        string username,
+        CompetitionStatus? competitionStatus = null,
+        CancellationToken cancellationToken = default
+    ) => await Try.ExecuteAsync(async () =>
+        await new HttpClientBuilder(client)
+            .WithRoute("players")
+            .WithRoute(username)
+            .WithRoute("competitions")
+            .WithRoute("standings")
+            .WithQueryParameter("status", competitionStatus?.ToString())
+            .ExecuteGetAsync(cancellationToken: cancellationToken)
+            .ToTypedResultAsync<IEnumerable<PlayerCompetitionStanding>>(cancellationToken)
+    ).FlattenAsync();
+
+    public async Task<Result<IEnumerable<PlayerMembership>>> GroupMemberships(
+        string username,
+        int? limit = null,
+        int? offset = null,
+        CancellationToken cancellationToken = default
+    ) => await Try.ExecuteAsync(async () =>
+        await new HttpClientBuilder(client)
+            .WithRoute("players")
+            .WithRoute(username)
+            .WithRoute("groups")
+            .WithQueryParameter("limit", limit)
+            .WithQueryParameter("offset", offset)
+            .ExecuteGetAsync(cancellationToken: cancellationToken)
+            .ToTypedResultAsync<IEnumerable<PlayerMembership>>(cancellationToken)
+    ).FlattenAsync();
+
+    public async Task<Result<PlayerProgressSnapshot>> Gains(
+        string username,
+        Period period,
+        CancellationToken cancellationToken = default
+    ) => await Try.ExecuteAsync(async () =>
+        await new HttpClientBuilder(client)
+            .WithRoute("players")
+            .WithRoute(username)
+            .WithRoute("gained")
+            .WithQueryParameter("period", period)
+            .ExecuteGetAsync(cancellationToken: cancellationToken)
+            .ToTypedResultAsync<PlayerProgressSnapshot>(cancellationToken)
+    ).FlattenAsync();
+
+    public async Task<Result<PlayerProgressSnapshot>> Gains(
+        string username,
+        DateTime startDate,
+        DateTime endDate,
+        CancellationToken cancellationToken = default
+    ) => await Try.ExecuteAsync(async () =>
+        await new HttpClientBuilder(client)
+            .WithRoute("players")
+            .WithRoute(username)
+            .WithRoute("gained")
+            .WithQueryParameter("startDate", startDate)
+            .WithQueryParameter("endDate", endDate)
+            .ExecuteGetAsync(cancellationToken: cancellationToken)
+            .ToTypedResultAsync<PlayerProgressSnapshot>(cancellationToken)
     ).FlattenAsync();
 }
